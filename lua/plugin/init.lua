6159916d03ecd0db468cda327a385e186ae091d4
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-08-08 23:38:39 -0700

    refactor: rewrote the logic to changing files

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -1,6 +1,31 @@
 local M = {}
 
-local ngRegex = ".(spec.ts|html|scss|sass|css|ts)$"
+local ngRegex = {
+  "%.component%.html$",
+  "%.component%.s?[ac]ss$",
+  "%.component%.ts$",
+  "%.component%.spec.ts$",
+  "%.directive%.ts$",
+  "%.guard%.ts$",
+  "%.service%.ts$",
+  "%.pipe%.ts$",
+  "%.directive%.spec%.ts$",
+  "%.guard%.spec%.ts$",
+  "%.service%.spec%.ts$",
+  "%.pipe%.spec%.ts$",
+}
+
+--- Determines if this is an Angular file
+---@param path string Full path of the file
+---@return boolean
+local function is_ng(path)
+    for _, ext in ipairs(ngRegex) do
+        if string.match(path, ext) then
+            return true
+        end
+    end
+    return false
+end
 
 --- Determines if the file is a test/spec file.
 ---@param path string Full path of the file
@@ -13,28 +38,33 @@ end
 ---@param path string Full path of the file
 ---@return boolean
 local function is_component(path)
-    return string.match(path, "%.component" .. ngRegex)
+    for i = 1, 4 do
+        if string.match(path, ngRegex[i]) then
+            return true
+        end
+    end
+    return false
 end
 
 --- Returns .css, .sass, or .scss of the Angular component
----@param path string Full path of the file
+---@param path string Path of the file, with the extension removed (.spec included)
 ---@return string|nil
 local function get_style_file(path)
-    if not string.match(path, "%.component" .. ngRegex) then
-        error(": not a component", 1)
+    if not is_component(path) then
+        print("Not a component")
         return nil
     end
 
-    local style_exts = { "css", "scss", "sass" }
+    local style_exts = { ".css", ".scss", ".sass" }
     for _, ext in ipairs(style_exts) do
-        local style_file = string.gsub(path, ngRegex, ext)
+        local style_file = path .. ext
         local files = vim.fn.findfile(style_file)
         if #files > 0 then
             return files[1]
         end
     end
 
-    error(": no style file found", 1)
+    print("No style file found")
     return nil
 end
 
@@ -43,18 +73,22 @@ end
 ---@param type string Type of file to open: `class`, `template`, `test` or `style`
 local function open_target_file(path, type)
     local file_to_open = nil
+    if is_spec(path) then
+        path = vim.fn.fnamemodify(path, ":r")
+    end
+    path = vim.fn.fnamemodify(path, ":r")
     if type == "class" then
-        file_to_open = string.gsub(path, ngRegex, ".ts")
+        file_to_open = path .. ".ts"
     elseif type == "template" then
-        file_to_open = string.gsub(path, ngRegex, ".html")
+        file_to_open = path .. ".html"
     elseif type == "test" then
-        file_to_open = string.gsub(path, ngRegex, ".spec.ts")
+        file_to_open = path .. ".spec.ts"
     elseif type == "style" then
         file_to_open = get_style_file(path)
     end
 
     if file_to_open == nil then
-        error(": unknown target file", 1)
+        print("Unknown target file")
         return
     end
 
@@ -63,6 +97,10 @@ end
 
 function M.quick_switch_toggle()
     local path = vim.fn.expand("%:p")
+    if not is_ng(path) then
+        print("Not an Angular file")
+        return
+    end
     -- Default is the class file
     local file_type = "class"
     local ext = vim.fn.fnamemodify(path, ":e")
@@ -80,19 +118,54 @@ function M.quick_switch_toggle()
 end
 
 function M.quick_switch_class()
-    open_target_file(vim.fn.expand("%:p"), "class")
+    local path = vim.fn.expand("%:p")
+    if not is_ng(path) then
+        print("Not an Angular file")
+        return
+    end
+    local ext = vim.fn.fnamemodify(path, ":e")
+    if ext == "ts" and not is_spec(path) then
+        return
+    end
+    open_target_file(path, "class")
 end
 
 function M.quick_switch_template()
-    open_target_file(vim.fn.expand("%:p"), "template")
+    local path = vim.fn.expand("%:p")
+    if not is_ng(path) then
+        print("Not an Angular file")
+        return
+    end
+    local ext = vim.fn.fnamemodify(path, ":e")
+    if ext == "html" then
+        return
+    end
+    open_target_file(path, "template")
 end
 
 function M.quick_switch_style()
-    open_target_file(vim.fn.expand("%:p"), "style")
+    local path = vim.fn.expand("%:p")
+    if not is_ng(path) then
+        print("Not an Angular file")
+        return
+    end
+    local ext = vim.fn.fnamemodify(path, ":e")
+    if string.match(ext, "s?[ac]ss") then
+        return
+    end
+    open_target_file(path, "style")
 end
 
 function M.quick_switch_test()
-    open_target_file(vim.fn.expand("%:p"), "test")
+    local path = vim.fn.expand("%:p")
+    if not is_ng(path) then
+        print("Not an Angular file")
+        return
+    end
+    if is_spec(path) then
+        return
+    end
+    open_target_file(path, "test")
 end
 
 function M.setup(opts)
@@ -101,7 +174,7 @@ function M.setup(opts)
     vim.api.nvim_create_user_command("NgQuickSwitchToggle", M.quick_switch_toggle, {})
     vim.api.nvim_create_user_command("NgQuickSwitchClass", M.quick_switch_class, {})
     vim.api.nvim_create_user_command("NgQuickSwitchTemplate", M.quick_switch_template, {})
-    vim.api.nvim_create_user_command("NgQuickSwitchTest", M.quick_switch_test, {})
+    vim.api.nvim_create_user_command("NgQuickSwitchSpec", M.quick_switch_test, {})
 
     if opts.use_default_keymaps then
         vim.keymap.set("n", "<leader>qs", ":NgQuickSwitchToggle<cr>")
